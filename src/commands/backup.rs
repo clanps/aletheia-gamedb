3057b69a7845ef06150e0821fc0fc6ff5327abbf
Spencer <git@spencer.moe>
2025-04-20 04:55:27 +0100

    Move expand_path to dirs module.

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -1,4 +1,4 @@
-use crate::dirs::config;
+use crate::dirs::expand_path;
 use crate::scanner::lutris::LutrisScanner;
 use crate::scanner::Scanner;
 use super::Command;
@@ -36,27 +36,5 @@ impl Command for Backup {
                 }
             }
         }
-    }
-}
-
-fn expand_path(path: &str, prefix: Option<&PathBuf>) -> PathBuf {
-    if cfg!(unix) {
-        let wine_prefix = prefix.unwrap();
-        let drive_c = wine_prefix.join("drive_c");
-        let users = drive_c.join("users").join("*"); // TODO: Ignore steamuser
-        let app_data = users.join("AppData");
-        let documents = users.join("Documents");
-
-        path
-            .replace("{AppData}", &app_data.display().to_string())
-            .replace("{Documents}", &documents.display().to_string())
-            .replace("{Home}", &users.display().to_string())
-            .replace("{LocalAppData}", &app_data.join("Local").display().to_string())
-            .replace("{LocalLow}", &app_data.join("LocalLow").display().to_string())
-            .replace("{SteamUserData}", "{SteamUserData}") // TODO
-            .replace("{XDGConfig}", &config().display().to_string())
-            .into()
-    } else {
-        todo!("Windows path expansion")
     }
 }
--- src/dirs.rs
+++ src/dirs.rs
@@ -12,3 +12,25 @@ pub fn config() -> std::path::PathBuf {
             .unwrap()
     }
 }
+
+pub fn expand_path(path: &str, prefix: Option<&PathBuf>) -> PathBuf {
+    if cfg!(unix) {
+        let wine_prefix = prefix.unwrap();
+        let drive_c = wine_prefix.join("drive_c");
+        let users = drive_c.join("users").join("*");
+        let app_data = users.join("AppData");
+        let documents = users.join("Documents");
+
+        path
+            .replace("{AppData}", &app_data.display().to_string())
+            .replace("{Documents}", &documents.display().to_string())
+            .replace("{Home}", &users.display().to_string())
+            .replace("{LocalAppData}", &app_data.join("Local").display().to_string())
+            .replace("{LocalLow}", &app_data.join("LocalLow").display().to_string())
+            .replace("{SteamUserData}", "{SteamUserData}") // TODO
+            .replace("{XDGConfig}", &config().display().to_string())
+            .into()
+    } else {
+        todo!("Windows path expansion")
+    }
+}
