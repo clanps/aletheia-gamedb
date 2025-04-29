aac025926eb590d3cc67d4b8ec860acf05f2f99a
Spencer <git@spencer.moe>
2025-04-29 23:38:07 +0100

    Refactor backup command.

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -4,7 +4,8 @@
 use crate::config::Config;
 use crate::dirs::{expand_path, shrink_path};
 use crate::file::hash_file;
-use crate::gamedb::{self, GameInfo, FileMetadata};
+use crate::gamedb::{self, GameDbEntry, GameInfo, FileMetadata};
+use crate::scanner::Game;
 use super::{Args, Command};
 use std::fs::{copy, create_dir_all, metadata, read_to_string, write};
 use std::path::PathBuf;
@@ -16,86 +17,89 @@ impl Command for Backup {
     fn run(args: Args, config: &Config) {
         let game_db = gamedb::parse();
         let installed_games = gamedb::get_installed_games();
-        let games: Vec<_> = if let Some(launcher) = args.get_flag_value("infer") {
+
+        if let Some(launcher) = args.get_flag_value("infer") {
             if launcher != "lutris" {
                 println!("Unsupported launcher, currently only Lutris is supported.");
                 return;
             }
 
-            let game_name = match std::env::var("GAME_NAME") {
-                Ok(name) => name,
-                Err(_) => {
-                    println!("GAME_NAME environment variable not found, is the game being launched by Lutris?");
-                    return;
-                }
+            let Ok(game_name) = std::env::var("GAME_NAME") else {
+                println!("GAME_NAME environment variable not found, is the game being launched by Lutris?");
+                return;
             };
 
-            installed_games.into_iter().filter(|game| game.name == game_name).collect()
-        } else if args.positional.is_empty() {
-            installed_games
+            if let Some(game) = installed_games.into_iter().find(|game| game.name == game_name) {
+                backup_game(&game, config, game_db.get(&game.name).unwrap());
+            }
+        } else if !args.positional.is_empty() {
+            for game in installed_games.into_iter().filter(|game| args.positional.contains(&game.name)).collect::<Vec<_>>() {
+                backup_game(&game, config, game_db.get(&game.name).unwrap());
+            }
         } else {
-            installed_games.into_iter().filter(|game| args.positional.contains(&game.name)).collect()
-        };
-
-        for game in games {
-            let backup_folder = PathBuf::from(&config.save_dir).join(&game.name);
-            let manifest_path = backup_folder.join("aletheia_manifest.yaml");
-            let mut changed = false;
-            let existing_manifest = manifest_path.exists().then(|| {
-                let content = read_to_string(&manifest_path).unwrap();
-                serde_yaml::from_str::<GameInfo>(&content).unwrap()
-            });
-
-            create_dir_all(&backup_folder).expect(&format!("Failed to backup {}.", game.name));
-
-            let game_entry = game_db.get(&game.name).unwrap();
-            let mut game_files: Vec<FileMetadata> = vec![];
-
-            if let Some(ref windows_paths) = game_entry.files.windows {
-                for path in windows_paths {
-                    let expanded = expand_path(path, Some(&game.directory));
-                    let found_paths = glob(&expanded.to_string_lossy()).unwrap();
-
-                    for file in found_paths {
-                        let file_path = file.unwrap();
-                        let file_path_str = file_path.to_string_lossy().to_string();
-                        let file_hash = hash_file(&file_path);
-                        let file_changed = existing_manifest.as_ref().is_none_or(|manifest| manifest.files.iter().find(|m| m.path == file_path_str).is_none_or(|metadata| metadata.hash != file_hash));
-
-                        if file_changed {
-                            let file_metadata = process_file(
-                                &file_path,
-                                &backup_folder.clone().join(file_path.file_name().unwrap()),
-                                Some(&game.directory)
-                            );
-
-                            game_files.push(file_metadata);
-                            changed = true;
-                        } else {
-                            let existing_metadata = existing_manifest.as_ref().unwrap().files
-                                .iter()
-                                .find(|m| m.path == file_path_str)
-                                .unwrap();
-
-                            game_files.push(existing_metadata.to_owned());
-                        }
-                    }
-                }
+            for game in installed_games {
+                backup_game(&game, config, game_db.get(&game.name).unwrap());
             }
-
-            if !changed {
-                continue;
-            }
-
-            let game_metadata = GameInfo {
-                name: game.name,
-                files: game_files
-            };
-
-            write(&manifest_path, serde_yaml::to_string(&game_metadata).unwrap()).unwrap();
-            println!("Backed up {}.", game_metadata.name);
         }
     }
+}
+
+fn backup_game(game: &Game, config: &Config, entry: &GameDbEntry) {
+    let backup_folder = PathBuf::from(&config.save_dir).join(&game.name);
+    let manifest_path = backup_folder.join("aletheia_manifest.yaml");
+    let mut changed = false;
+    let existing_manifest = manifest_path.exists().then(|| {
+        let content = read_to_string(&manifest_path).unwrap();
+        serde_yaml::from_str::<GameInfo>(&content).unwrap()
+    });
+
+    create_dir_all(&backup_folder).expect(&format!("Failed to backup {}.", game.name));
+
+    let mut game_files: Vec<FileMetadata> = vec![];
+
+    if let Some(ref windows_paths) = entry.files.windows {
+        for path in windows_paths {
+            let expanded = expand_path(path, Some(&game.directory));
+            let found_paths = glob(&expanded.to_string_lossy()).unwrap();
+
+            for file in found_paths {
+                let file_path = file.unwrap();
+                let file_path_str = file_path.to_string_lossy().to_string();
+                let file_hash = hash_file(&file_path);
+                let file_changed = existing_manifest.as_ref().is_none_or(|manifest| manifest.files.iter().find(|m| m.path == file_path_str).is_none_or(|metadata| metadata.hash != file_hash));
+
+                if file_changed {
+                    let file_metadata = process_file(
+                        &file_path,
+                        &backup_folder.clone().join(file_path.file_name().unwrap()),
+                        Some(&game.directory)
+                    );
+
+                    game_files.push(file_metadata);
+                    changed = true;
+                } else {
+                    let existing_metadata = existing_manifest.as_ref().unwrap().files
+                        .iter()
+                        .find(|m| m.path == file_path_str)
+                        .unwrap();
+
+                    game_files.push(existing_metadata.to_owned());
+                }
+            }
+        }
+    }
+
+    if !changed {
+        return;
+    }
+
+    let game_metadata = GameInfo {
+        name: game.name.clone(),
+        files: game_files
+    };
+
+    write(&manifest_path, serde_yaml::to_string(&game_metadata).unwrap()).unwrap();
+    println!("Backed up {}.", game_metadata.name);
 }
 
 fn process_file(file_path: &PathBuf, dest: &PathBuf, prefix: Option<&PathBuf>) -> FileMetadata {
