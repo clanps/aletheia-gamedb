1a162e91abfb9aac0d92fc2bfb7b2c059ea56f27
Spencer <git@spencer.moe>
2025-04-29 09:07:39 +0100

    LutrisScanner: Prevent crash if a game has a malformed config.

--- src/scanner/lutris.rs
+++ src/scanner/lutris.rs
@@ -34,7 +34,11 @@ impl Scanner for LutrisScanner {
             }
 
             let file = File::open(&path).unwrap();
-            let yml: serde_yaml::Value = serde_yaml::from_reader(file).unwrap();
+
+            let yml: serde_yaml::Value = match serde_yaml::from_reader(file) {
+                Ok(y) => y,
+                Err(_) => continue
+            };
 
             let name = yml.get("name")
                 .and_then(|n| n.as_str());
