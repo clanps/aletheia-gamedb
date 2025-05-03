66ed4ea4650d175a5818d3aa0b835f01b05362b9
Spencer <git@spencer.moe>
2025-05-03 01:04:36 +0100

    Backup: Fix NTFS errors.

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -43,7 +43,12 @@ impl Command for Backup {
 }
 
 fn backup_game(game: &Game, config: &Config, entry: &GameDbEntry) {
-    let backup_folder = PathBuf::from(&config.save_dir).join(&game.name);
+    let backup_folder = if cfg!(unix) {
+        PathBuf::from(&config.save_dir).join(&game.name)
+    } else {
+        PathBuf::from(&config.save_dir).join(game.name.replace(':', ""))
+    };
+
     let manifest_path = backup_folder.join("aletheia_manifest.yaml");
     let mut changed = false;
     let existing_manifest = manifest_path.exists().then(|| {
