125c0794fac3830b73d95a7776d0d1f86eebaba8
Spencer <git@spencer.moe>
2025-05-24 05:18:16 +0100

    Lutris: Support GameRoot.

--- src/scanner/lutris.rs
+++ src/scanner/lutris.rs
@@ -1,16 +1,32 @@
 // SPDX-FileCopyrightText: 2025 Spencer
 // SPDX-License-Identifier: AGPL-3.0-only
 
-use crate::dirs::{app_data, home};
+use crate::dirs::{app_data, config, home};
 use super::{Game, Scanner};
+use std::path::PathBuf;
 
 pub struct LutrisScanner;
 
 impl Scanner for LutrisScanner {
     fn get_games() -> Vec<Game> {
         let mut games = vec![];
+
+        let lutris_config_dir_deprecated = config().join("lutris/games");
+        let lutris_config_dir_new = app_data().join("lutris/games");
+        let lutris_config_dir_flatpak = home().join(".var/app/net.lutris.Lutris/data/lutris/games");
+
         let lutris_db_path = app_data().join("lutris/pga.db");
         let lutris_db_path_flatpak = home().join(".var/app/net.lutris.Lutris/data/lutris/pga.db");
+
+        let lutris_config_dir = if lutris_config_dir_deprecated.exists() {
+            lutris_config_dir_deprecated
+        } else if lutris_config_dir_new.exists() {
+            lutris_config_dir_new
+        } else if lutris_config_dir_flatpak.exists() {
+            lutris_config_dir_flatpak
+        } else {
+            return games;
+        };
 
         let con = if lutris_db_path.exists() {
             rusqlite::Connection::open(lutris_db_path).unwrap()
@@ -20,25 +36,34 @@ impl Scanner for LutrisScanner {
             return games;
         };
 
-        let mut stmt = con.prepare("SELECT name, directory, platform FROM games").unwrap();
+        let mut stmt = con.prepare("SELECT name, directory, platform, configpath FROM games").unwrap();
         let rows = stmt
             .query_map([], |row| {
                 let name: String = row.get(0)?;
                 let path: String = row.get(1)?;
                 let platform: String = row.get(2)?;
-                Ok((name, std::path::PathBuf::from(path), platform))
+                let config_path: String = row.get(3)?;
+                Ok((name, PathBuf::from(path), platform, lutris_config_dir.join(format!("{config_path}.yml"))))
             })
             .unwrap();
 
         for row in rows {
-            let (name, dir, platform) = row.unwrap();
+            let (name, dir, platform, config_file) = row.unwrap();
 
             if !dir.as_os_str().is_empty() && !dir.exists() { // Lutris leaves some directories empty (not sure why) and doesn't seem to remove the directory from the database after the game is uninstalled
                 continue;
             }
 
             if platform == "Windows" {
-                games.push(Game { name, installation_dir: None, prefix: Some(dir), source: "Lutris".into() });
+                let game_config: serde_yaml::Value = serde_yaml::from_reader(std::fs::File::open(config_file).unwrap()).unwrap();
+                let installation_dir = game_config
+                    .get("game")
+                    .and_then(|g| g.get("exe"))
+                    .and_then(|e| e.as_str())
+                    .map(PathBuf::from)
+                    .map(|p| p.parent().unwrap().to_path_buf());
+
+                games.push(Game { name, installation_dir, prefix: Some(dir), source: "Lutris".into() });
             } else {
                 games.push(Game { name, installation_dir: Some(dir), prefix: None, source: "Lutris".into() });
             }
