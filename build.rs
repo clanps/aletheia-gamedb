c05b5259dd8fd3381ed3bd7f1d421cc024c46ee1
Spencer <git@spencer.moe>
2025-06-20 13:33:03 +0100

    UI: Fix inverted colours on light mode desktops.

--- build.rs
+++ build.rs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 fn main() {
-    let config = slint_build::CompilerConfiguration::new();
+    let config = slint_build::CompilerConfiguration::new()
+        .with_style("material-dark".into());
 
     slint_build::compile_with_config("ui/app.slint", config).expect("Slint build failed.");
 
