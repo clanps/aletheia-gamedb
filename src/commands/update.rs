fa18cb72ba1560527f58e439b6938a84f34fa4a5
Spencer <git@spencer.moe>
2025-06-24 16:49:18 +0100

    Update: Use release URL instead of hardcoded latest.

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -10,7 +10,7 @@ pub struct Update;
 impl Command for Update {
     fn run(_args: Args, _config: &Config) {
         match updater::check() {
-            Ok(updater::UpdateStatus::Available(_)) => println!("Aletheia is out of date! You can download the newest release here: https://github.com/Spencer-0003/aletheia/releases/latest"),
+            Ok(updater::UpdateStatus::Available(r)) => println!("Aletheia is out of date! You can download the newest release here: {}", r.url),
             Ok(updater::UpdateStatus::UpToDate) => println!("Aletheia is already up to date."),
             Err(e) => eprintln!("Error checking for updates: {e}")
         }
