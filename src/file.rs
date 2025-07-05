b6fedbd52bad9421e7908de540e0686636568345
Spencer <git@spencer.moe>
2025-07-05 22:38:07 +0100

    File: Use Path instead of PathBuf.

--- src/file.rs
+++ src/file.rs
@@ -2,12 +2,15 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 use sha2::{Sha512, Digest};
+use std::fs::File;
+use std::io::copy;
+use std::path::Path;
 
-pub fn hash_file(file_path: &std::path::PathBuf) -> String {
-    let mut file_content = std::fs::File::open(file_path).unwrap();
+pub fn hash_file(file_path: &Path) -> String {
+    let mut file_content = File::open(file_path).unwrap();
     let mut hasher = Sha512::new();
 
-    std::io::copy(&mut file_content, &mut hasher).unwrap();
+    copy(&mut file_content, &mut hasher).unwrap();
 
     format!("{:x}", hasher.finalize())
 }
