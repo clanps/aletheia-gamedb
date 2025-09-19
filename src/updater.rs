c6f420ee66d8d23a6f850f2287ce246a4607994e
Spencer <git@spencer.moe>
2025-09-19 12:56:00 +0100

    Updater: Code clean up.

--- src/updater.rs
+++ src/updater.rs
@@ -1,6 +1,12 @@
 // SPDX-FileCopyrightText: 2025 Spencer
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use reqwest::{blocking::Client, header};
+use semver::Version;
+
+const RELEASES_URL: &str = "https://api.github.com/repos/Spencer-0003/aletheia/releases";
+const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
 #[derive(Clone, serde::Deserialize)]
 pub struct Release {
     pub body: String,
@@ -27,20 +33,16 @@ pub enum UpdateStatus {
 }
 
 pub fn check() -> Result<UpdateStatus> {
-    let client = reqwest::blocking::Client::new();
-    let response = client
-        .get("https://api.github.com/repos/Spencer-0003/aletheia/releases")
-        .header(reqwest::header::USER_AGENT, concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")))
-        .send()?
-        .error_for_status()?;
+    let client = Client::new();
+    let response = client.get(RELEASES_URL).header(header::USER_AGENT, USER_AGENT).send()?.error_for_status()?;
 
     let releases: Vec<Release> = serde_json::from_reader(response)?;
     let Some(latest_release) = releases.iter().find(|r| !r.pre_release) else {
         return Ok(UpdateStatus::UpToDate);
     };
 
-    let current_version = semver::Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
-    let new_version = semver::Version::parse(&latest_release.tag_name).unwrap();
+    let current_version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
+    let new_version = Version::parse(&latest_release.tag_name).unwrap();
 
     if current_version < new_version {
         Ok(UpdateStatus::Available(latest_release.clone()))
